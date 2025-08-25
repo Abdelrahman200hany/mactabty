bdd658af27c3161190ed138de1127376e4e6fe02
Abdelrahman Hany <abdelrahmanhany20024@gmail.com>
2025-08-25 18:51:50 +0300

    create failure file and use  dartz package to write the data type og home repo

--- lib/feature/home/data/repos/home_repo.dart
+++ lib/feature/home/data/repos/home_repo.dart
@@ -1,7 +1,8 @@
+import 'package:mactabty/core/errors/failute.dart';
+import 'package:mactabty/feature/home/data/models/book_moldel/book_moldel.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-
-fectchFreeSellerBox();
-fectchFeatureBox();
-
-
+  Future<Either<Failure, List<BookMoldel>>> fectchFreeSellerBox();
+  Future<Either<Failure, List<BookMoldel>>> fectchFeatureBox();
 }
